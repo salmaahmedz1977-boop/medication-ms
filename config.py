0bd3c09fa7942ab92874db94dc34e5b571f98cc4
salmaahmedz1977-boop <salmaahmedz1977@gmail.com>
2026-05-19 06:16:44 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,12 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(24))
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://root:asdfghjkl@localhost/medication_system')
+    
+    db_url = os.environ.get('DATABASE_URL')
+    if db_url:
+        # Railway gives mysql://..., but we need mysql+pymysql://
+        db_url = db_url.replace('mysql://', 'mysql+pymysql://', 1)
+    SQLALCHEMY_DATABASE_URI = db_url or 'mysql+pymysql://root:asdfghjkl@localhost/medication_system'
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
